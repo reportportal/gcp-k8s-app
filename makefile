49caf65152c6693a991f2f2c8a4b8f84062a570a
raikbitters <raikbitters@gmail.com>
2023-12-08 20:17:20 +0400

    Delete unnecessary files and dependencies

--- makefile
+++ makefile
@@ -1,24 +1,14 @@
 # Description: Makefile for ReportPortal GCP Marketplace application
 registry := gcr.io/$(shell gcloud config get-value project | tr ':' '/')
 app_name := reportportal
-release_version := $(shell grep '^version:' chart/reportportal-k8s-app/Chart.yaml | awk '{print $$2}')
-release_track := $(shell echo $(release_version) | cut -d '.' -f 1,2)
-dependency_chart_version := $(shell grep 'version:' chart/reportportal-k8s-app/Chart.yaml | tail -n 1 | awk '{print $$2}')
+release_version := $(shell grep '^version:' data/chart/reportportal-k8s-app/Chart.yaml | awk '{print $$2}')
+release_track := $(shell grep '^appVersion:' data/chart/reportportal-k8s-app/Chart.yaml | awk '{print $$2}')
+dependency_chart_version := $(shell grep 'version:' data/chart/reportportal-k8s-app/Chart.yaml | tail -n 1 | awk '{print $$2}')
 deployer_image := $(registry)/$(app_name)/deployer
 values_path := tmp/general-values.yaml
 cluster_name := rp-mp-test-cluster
 cluster_location := us-central1
 namespace := test-ns
-
-# Infrastucture images
-postgres_origin_image := bitnami/postgresql:11.22.0
-postgres_rp_gcr_image := $(registry)/$(app_name)/postgresql11
-rabbitmq_origin_image := bitnami/rabbitmq:3.10.25
-rabbitmq_rp_gcr_image := $(registry)/$(app_name)/rabbitmq3
-opensearch_origin_image := opensearchproject/opensearch:2.11.1
-opensearch_rp_gcr_image := $(registry)/$(app_name)/opensearch2
-minio_origin_image := bitnami/minio:2023.11.20
-minio_rp_gcr_image := $(registry)/$(app_name)/minio2023
 
 # Default target.
 default: push
@@ -58,11 +48,11 @@ verify:
 build: show-versions
 	@echo
 	@echo "Building image $(deployer_image)"
-	@helm dependency build chart/reportportal-k8s-app
+	@helm dependency build data/chart/reportportal-k8s-app
 	@docker build \
 		--build-arg REGISTRY=$(registry)/$(app_name) \
 		--build-arg TAG=$(release_version) \
-		--tag $(deployer_image):$(release_track) .
+		--tag $(deployer_image):$(release_track) ./data/
 	@docker tag $(deployer_image):$(release_track) $(deployer_image):$(release_version)
 
 # Pushes a Deployer Docker image to your Google Cloud Registry.
@@ -73,47 +63,16 @@ push: configure-docker build
 	@docker push $(deployer_image):$(release_version)
 
 # Publishing used Chart ReportPortal images from Docker Hub to GCR
-publish-images: show-versions configure-docker
-	@echo
 	@echo "Getting values from dependency chart..."
-	@helm dependency build chart/reportportal-k8s-app
-	@helm inspect values ./chart/reportportal-k8s-app/charts/reportportal-$(dependency_chart_version).tgz > $(values_path)
+	@helm dependency build data/chart/reportportal-k8s-app
+	@helm inspect values data/chart/reportportal-k8s-app/charts/reportportal-$(dependency_chart_version).tgz > $(values_path)
+	@echo
+	
+publish: show-versions configure-docker
 	@echo
 	@echo "Running publishing images..."
-	-@python scripts/publish-gcr.py \
-		--values-path $(values_path) \
-		--release-track $(release_track) \
-		--release-version $(release_version)
+	@VALUES_PATH=$(values_path) RELEASE_TRACK=$(release_track) RELEASE_VERSION=$(release_version) \
+		python scripts/publish-gcr.py
 
-# Publishing used Chart Infrastucture images from Docker Hub to GCR
-publish-dependency: configure-docker
-	@echo
-	@echo "publishing Postgresql..."
-	@docker pull $(postgres_origin_image)
-	@docker tag $(postgres_origin_image) $(postgres_rp_gcr_image):$(release_track)
-	@docker tag $(postgres_origin_image) $(postgres_rp_gcr_image):$(release_version)
-	@docker push $(postgres_rp_gcr_image):$(release_track)
-	@docker push $(postgres_rp_gcr_image):$(release_version)
-	@echo
-	@echo "publishing RabbitMQ..."
-	@docker pull $(rabbitmq_origin_image)
-	@docker tag $(rabbitmq_origin_image) $(rabbitmq_rp_gcr_image):$(release_track)
-	@docker tag $(rabbitmq_origin_image) $(rabbitmq_rp_gcr_image):$(release_version)
-	@docker push $(rabbitmq_rp_gcr_image):$(release_track)
-	@docker push $(rabbitmq_rp_gcr_image):$(release_version)
-	@echo
-	@echo "publishing OpenSearch..."
-	@docker pull $(opensearch_origin_image)
-	@docker tag $(opensearch_origin_image) $(opensearch_rp_gcr_image):$(release_track)
-	@docker tag $(opensearch_origin_image) $(opensearch_rp_gcr_image):$(release_version)
-	@docker push $(opensearch_rp_gcr_image):$(release_track)
-	@docker push $(opensearch_rp_gcr_image):$(release_version)
-	@echo
-	@echo "publishing Minio..."
-	@docker pull $(minio_origin_image)
-	@docker tag $(minio_origin_image) $(minio_rp_gcr_image):$(release_track)
-	@docker tag $(minio_origin_image) $(minio_rp_gcr_image):$(release_version)
-	@docker push $(minio_rp_gcr_image):$(release_track)
-	@docker push $(minio_rp_gcr_image):$(release_version)
 
-publish-all: push publish-images publish-dependency
+publish-all: push publish
--- scripts/publish-gcr.py
+++ scripts/publish-gcr.py
@@ -1,71 +1,75 @@
 import yaml
+import os
 import subprocess
-import argparse
-import yaml
 
-# Create the parser
-parser = argparse.ArgumentParser(description="Script for publishing used chart images to GCR")
+# Get the environment variables
+values_file_path = os.getenv("VALUES_PATH")
+schema_file_path = os.getenv("SCHEMA_PATH", "data/schema.yaml")
+release_track = os.getenv("RELEASE_TRACK")
+release_version = os.getenv("RELEASE_VERSION")
+primary_image_service = os.getenv("PRIMARY_SERVICE", "")
+old_repo = os.getenv("OLD_REPO", "reportportal")
+new_registry = os.getenv("NEW_REGISTRY", "gcr.io")
+new_repo = os.getenv("NEW_REPO", "epam-mp-rp/reportportal")
 
-# Add the arguments
-parser.add_argument('--values-path', type=str, required=True, help='Path to the chart values.yaml file')
-parser.add_argument('--release-track' , type=str, required=True, help='Release track')
-parser.add_argument('--release-version', type=str, required=True, help='Release version')
-parser.add_argument('--primary-service', type=str, required=False, default='serviceapi', help='Primary service')
-parser.add_argument('--old-repo', type=str, required=False, default='reportportal', help='Old repository')
-parser.add_argument('--new-registry', type=str, required=False, default='gcr.io', help='New registry')
-parser.add_argument('--new-repo', type=str, required=False, default='epam-mp-rp/reportportal', help='New repository with registry')
+def load_yaml(file_path):
+    with open(file_path, "r") as file:
+        return yaml.safe_load(file)
 
-# Parse the arguments
-args = parser.parse_args()
+def construct_image_name(registry, repository, tag):
+    return (
+        f"{registry}/{repository}:{tag}"
+        if registry and not registry.endswith("/")
+        else f"{registry}{repository}:{tag}"
+    )
+    
 
-# Define the new registry
-values_file_path = args.values_path
-release_track = args.release_track
-release_version = args.release_version
-primary_image_service = args.primary_service
-old_repo = args.old_repo
-new_registry = args.new_registry
-new_repo = args.new_repo
-
-# Load the schema.yaml file for getting the image names for gcr.io
-with open('schema.yaml', 'r') as file:
-    schema_file = yaml.safe_load(file)
-
-# Load the values.yaml file for getting the image repository and tag
-with open(values_file_path, 'r') as file:
-    values_file = yaml.safe_load(file)
+schema_file = load_yaml(schema_file_path)
+values_file = load_yaml(values_file_path)
 
 # Iterate over the images in schema.yaml
-for gcr_image_name, value in schema_file['x-google-marketplace']['images'].items():
+for gcr_image_name, value in schema_file["x-google-marketplace"]["images"].items():
     # Get the repository and tag
-    properties = value['properties']
-    keys = list (properties.keys())
-    chart_value = keys[0].split('.')[1]
+    properties = value["properties"]
+    keys = list(properties.keys())
+    chart_value = keys[0].split(".")[1]
 
     for values_key, value in values_file.items():
         if values_key == chart_value:
-            # Get the repository and tag
-            if isinstance(value, dict) and 'repository' in value:
-                repository = value['repository']
-                tag = value['tag']
+            # Get a registry, repository and tag
+            if isinstance(value, dict) and "image" in value:
+                image = value["image"]
+                registry = image.get("registry", "")
+                repository = image.get("repository", "")
+                tag = image.get("tag", "")
 
                 # Construct the old and new image names
+                old_image = construct_image_name(registry, repository, tag)
+
                 if values_key == primary_image_service:
-                    old_image = f'{repository}:{tag}'
-                    new_repository = f'{new_registry}/{new_repo}'
-                    new_image_track = f'{new_repository}:{release_track}'
-                    new_image_version = f'{new_repository}:{release_version}'
+                    new_image_track = construct_image_name(new_registry, new_repo, release_track)
+                    new_image_version = construct_image_name(new_registry, new_repo, release_version)
                 else:
-                    old_image = f'{repository}:{tag}'
-                    new_repository = f'{new_registry}/{new_repo}/{gcr_image_name}'
-                    new_image_track = f'{new_repository}:{release_track}'
-                    new_image_version = f'{new_repository}:{release_version}'
+                    new_image_track =  construct_image_name(new_registry, f"{new_repo}/{gcr_image_name}", release_track)
+                    new_image_version =  construct_image_name(new_registry, f"{new_repo}/{gcr_image_name}", release_version)
 
                 # Pull the old image, tag it with the new image names, and push them to the new registry
                 print(f'Key: {values_key}\nOld_image: {old_image}\nNew_image_track: {new_image_track}\nNew_image_version: {new_image_version}')
-                subprocess.run(['docker', 'pull', old_image])
-                subprocess.run(['docker', 'tag', old_image, new_image_track])
-                subprocess.run(['docker', 'tag', old_image, new_image_version])
-                subprocess.run(['docker', 'push', new_image_track])
-                subprocess.run(['docker', 'push', new_image_version])
+                
+                docker_commands = [
+                    ['docker', 'pull', old_image],
+                    ['docker', 'tag', old_image, new_image_track],
+                    ['docker', 'tag', old_image, new_image_version],
+                    ['docker', 'push', new_image_track],
+                    ['docker', 'push', new_image_version]
+                ]
+
+                for command in docker_commands:
+                    result = subprocess.run(command, stdout=subprocess.PIPE)
+                    if result.returncode != 0:
+                        print(f'Command: {command}\nError: {result.stdout.decode("utf-8")}')
+                        exit(1)
+                    else:
+                        print(f'Command: {command}\nSuccess: {result.stdout.decode("utf-8")}')
+                
                 print(f'Image: {new_image_track} and {new_image_version} pushed to {new_registry}\n')
